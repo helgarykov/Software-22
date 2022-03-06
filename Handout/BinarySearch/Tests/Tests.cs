b377d486bf068c66377e4362de695d0507c93b70
Helga Rykov Ibsen <92053112+helgarykov@users.noreply.github.com>
2022-03-06 10:21:49 +0100

    Assignment 5

---
+++ Handout/BinarySearch/Tests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using Library;
+using NUnit.Framework;
+using PersonNS;
+
+namespace Tests {
+    [TestFixture]
+    public class Test {
+        [SetUp]
+        public void Init() {
+            gen = new Generator();
+        }
+
+        private Generator gen;
+
+        [Test]
+        public void DummyTest() {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
