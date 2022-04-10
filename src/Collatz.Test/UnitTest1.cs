349f5731bdecade38d9d93b7e17ea363c3b3593a
Helga Rykov Ibsen <92053112+helgarykov@users.noreply.github.com>
2022-04-10 16:30:51 +0200

    New Project

---
+++ src/Collatz.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Collatz.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
