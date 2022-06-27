53949783978c271f65d4d5f48aa56fb3c3212fa8
Helga Rykov Ibsen <92053112+helgarykov@users.noreply.github.com>
2022-06-27 14:37:11 +0200

    Created Project

---
+++ sommer/Lecture1/Lecture1.Application/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Lecture1
+{
+    public class Program
+    {
+        public enum Season
+        {
+            Spring,
+            Sommer, 
+            Autumn, 
+            Winter
+        }
+        public void Main(string[] args)
+        {
+            bool isGoldCustomer = true;
+            //float price = 19.95f;
+
+            /* if (isGoldCustomer)
+            {
+                price = 19.95f;
+            }
+            else
+            {
+                price = 29.95f;
+            } */
+
+            float price = (isGoldCustomer) ? 19.95f : 29.95f; // the same as is/else statement above
+            Console.WriteLine(price);
+        }
+        
+    }
+
+}
---
+++ sommer/Lecture1/Lecture1.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Lecture1.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
